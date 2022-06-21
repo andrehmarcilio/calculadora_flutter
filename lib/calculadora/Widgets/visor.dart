56c4717a337c51f144fe1e660fc5a411d8e1c3a3
André Marcílio <andrehmarcilio@gmail.com>
2022-06-20 21:02:46 -0300

    visualização da conta anterior

--- lib/calculadora/Widgets/visor.dart
+++ lib/calculadora/Widgets/visor.dart
@@ -14,18 +14,52 @@ class Visor extends StatelessWidget {
         alignment: Alignment.bottomRight,
         color: const Color.fromARGB(255, 11, 1, 1),
         child: BlocBuilder<CalculadoraBloc, CalculadoraEstados>(
-          builder: (_, state) => Text(
-            state.conta,
-            textAlign: TextAlign.end,
-            style: TextStyle(
-                fontSize: state.conta.length < 5
-                    ? 80.0
-                    : 80.0 - state.conta.length * 2.5 < 40
-                        ? 40
-                        : 80.0 - state.conta.length * 2.5,
-                fontWeight: FontWeight.w300,
-                color: Colors.white),
-          ),
+          builder: (_, state) {
+            if (state is MostrandoResultado) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    state.resultado,
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                        fontSize: state.conta.length < 5
+                            ? 60.0
+                            : 60.0 - state.conta.length * 2.5 < 40
+                                ? 40
+                                : 60.0 - state.conta.length * 2.5,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    state.conta,
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                        fontSize: state.conta.length < 5
+                            ? 80.0
+                            : 80.0 - state.conta.length * 2.5 < 40
+                                ? 40
+                                : 80.0 - state.conta.length * 2.5,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white),
+                  )
+                ],
+              );
+            }
+            return Text(
+              state.conta,
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                  fontSize: state.conta.length < 5
+                      ? 80.0
+                      : 80.0 - state.conta.length * 2.5 < 40
+                          ? 40
+                          : 80.0 - state.conta.length * 2.5,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white),
+            );
+          },
         ),
       ),
     );
